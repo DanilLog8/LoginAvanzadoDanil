f2a6f85207240d06104df5014f9e613065643888
DanilLog8 <loginov.danil@iescamas.es>
2023-12-15 14:00:48 +0100

    first commit

--- php/conexion_be.php
+++ php/conexion_be.php
@@ -1,6 +1,6 @@
 <?php
 
-$conexion = mysqli_connect("localhost", "root", "", "login_register_db");
+$conexion = mysqli_connect("localhost", "myloginova6", "ksIbCkYS", "loginavanzado300");
 
 /*/
 if ($conexion){
