d622ce2604346303cf004bd49fdf586be420a54a
DanilLog8 <132345471+DanilLog8@users.noreply.github.com>
2023-12-15 13:39:13 +0100

    Add files via upload

---
+++ LoginAvanzado/php/conexion_be.php
@@ -0,0 +1,13 @@
+<?php
+
+$conexion = mysqli_connect("localhost", "myloginova6", "ksIbCkYS", "loginavanzado300");
+
+/*/
+if ($conexion){
+    echo "conectado";
+}else{
+        echo "no conectado";
+}
+*/
+
+?>
